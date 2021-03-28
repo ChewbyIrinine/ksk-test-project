c350426ab860ed74f5cbacada0abbf7bc733ff27
Alexander Korchagin <al_kor_00@bk.ru>
2021-03-28 18:56:39 +0300

    Centering content and header optimization

--- src/App/App.js
+++ src/App/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import Header from "../Header";
+//import Header from "../Header";
 import Filters from "../Filters";
 import Sorting from "../Sorting";
 import CardsField from "../CardsField";
@@ -12,17 +12,19 @@ export default class App extends React.Component {
 	render() {
 		return (
 			<div className="app">
-				<div className="header">
-					<Header />
-				</div>
-				<div className="filters">
-					<Filters />
-				</div>
-				<div className="sorting">
-					<Sorting />
-				</div>
-				<div className="cards-field">
-					<CardsField />
+				<header className="header">
+					<h1 className="header__label">Тестовое задание</h1>
+				</header>
+				<div className="central-content">
+					<div className="filters">
+						<Filters />
+					</div>
+					<div className="sorting">
+						<Sorting />
+					</div>
+					<div className="cards-field">
+						<CardsField />
+					</div>
 				</div>
 				<div className="footer">
 					<Footer />
