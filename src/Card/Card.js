7e9059f9e383da88da54c74203a1fece8ccae931
Alexander Korchagin <al_kor_00@bk.ru>
2021-03-30 21:47:21 +0300

    Stylized cards

--- src/Card/Card.js
+++ src/Card/Card.js
@@ -1,11 +1,27 @@
 import React from "react";
 
+import "./Card.css";
+
 const Card = () => {
 	return (
-		<div>
-			<h2>Card 1</h2>
-			<label>A</label>
-			<label>12.03.2021</label>
+		<div className="card">
+			<div className="card__heading">
+				<h2 className="card__title">Card 1</h2>
+			</div>
+			<div className="card__content">
+				<label className="card__id">A</label>
+				<hr />
+				<label className="card__creation-date">12.03.2021</label>
+				<hr />
+				<label className="card__invoice-number">1</label>
+				<hr />
+				<label className="card__arrival-time">
+					from 30.03.2021 by 31.03.2021
+				</label>
+				<hr />
+				<label className="card__type">RUED</label>
+				<hr />
+			</div>
 		</div>
 	);
 };
