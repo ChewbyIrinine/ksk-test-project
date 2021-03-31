9a4687b791ae7465c31a053ab1f21430141e2aec
Alexander Korchagin <al_kor_00@bk.ru>
2021-03-31 15:43:37 +0300

    Arrival date filter

--- src/App/App.js
+++ src/App/App.js
@@ -13,48 +13,82 @@ export default class App extends React.Component {
 		items: [
 			{
 				id: 1,
-				creationDate: "2021-3-12",
+				creationDate: "2021-03-12",
 				invoiceNumber: 1,
-				arrivalTimeFrom: "2021-3-30",
-				arrivalTimeBy: "2021-3-31",
+				arrivalTimeFrom: "2021-03-30",
+				arrivalTimeBy: "2021-03-31",
 				type: "RUED",
 			},
 			{
 				id: 2,
 				creationDate: "2020-12-07",
 				invoiceNumber: 1,
-				arrivalTimeFrom: "2021-3-30",
-				arrivalTimeBy: "2021-3-31",
+				arrivalTimeFrom: "2020-03-05",
+				arrivalTimeBy: "2020-03-10",
 				type: "RUEX",
 			},
 			{
 				id: 3,
 				creationDate: "2020-10-02",
 				invoiceNumber: 1,
-				arrivalTimeFrom: "2021-3-30",
-				arrivalTimeBy: "2021-3-31",
+				arrivalTimeFrom: "2021-01-03",
+				arrivalTimeBy: "2021-01-07",
 				type: "RUSG",
 			},
 			{
 				id: 4,
 				creationDate: "2021-01-13",
 				invoiceNumber: 1,
-				arrivalTimeFrom: "2021-3-30",
-				arrivalTimeBy: "2021-3-31",
+				arrivalTimeFrom: "2021-05-31",
+				arrivalTimeBy: "2021-06-03",
 				type: "RUED",
 			},
 			{
 				id: 5,
-				creationDate: "2021-2-05",
+				creationDate: "2021-02-05",
 				invoiceNumber: 1,
-				arrivalTimeFrom: "2021-3-30",
-				arrivalTimeBy: "2021-3-31",
+				arrivalTimeFrom: "2021-04-11",
+				arrivalTimeBy: "2021-04-12",
 				type: "RUEX",
 			},
 		],
+
+		filterArrivalFrom: "",
+		filterArrivalBy: "",
+	};
+
+	filter(items, filterArrivalFrom, filterArrivalBy) {
+		let filteredArr = items;
+		console.log(filteredArr);
+		if (filterArrivalFrom !== "") {
+			filteredArr = filteredArr.filter(
+				(item) => item.arrivalTimeFrom >= filterArrivalFrom
+			);
+		}
+		console.log(filteredArr);
+		if (filterArrivalBy !== "") {
+			filteredArr = filteredArr.filter(
+				(item) => item.arrivalTimeBy <= filterArrivalBy
+			);
+		}
+		console.log(filteredArr);
+		return filteredArr;
+	}
+
+	onFilterChange = (filterArrivalFrom, filterArrivalBy) => {
+		this.setState({ filterArrivalFrom, filterArrivalBy });
+		console.log("Изменение фильтра");
 	};
 
 	render() {
+		const { items, filterArrivalFrom, filterArrivalBy } = this.state;
+
+		const visibleItems = this.filter(
+			items,
+			filterArrivalFrom,
+			filterArrivalBy
+		);
+
 		return (
 			<div className="app">
 				<header className="header">
@@ -79,9 +113,13 @@ export default class App extends React.Component {
 						>
 							ДОБАВИТЬ
 						</button>
-						<Filters />
+						<Filters
+							filterArrivalFrom={filterArrivalFrom}
+							filterArrivalBy={filterArrivalBy}
+							onFilterChange={this.onFilterChange}
+						/>
 						<Sorting />
-						<CardsField items={this.state.items} />
+						<CardsField items={visibleItems} />
 					</div>
 				</div>
 				<footer className="footer"></footer>
--- src/Filters/Filters.js
+++ src/Filters/Filters.js
@@ -2,59 +2,81 @@ import React from "react";
 
 import "./Filters.css";
 
-const Filters = () => {
-	return (
-		<div className="filters">
-			<div className="filters__heading">
-				<h2 className="filters-text filters__heading-label">Filters</h2>
-			</div>
-			<div className="filters__content">
-				<label className="filters-text filters__content-label">
-					ID
-				</label>
-				<input
-					className="form-control filters__input"
-					type="text"
-					placeholder="Enter value"
-				></input>
-				<label className="filters-text filters__content-label">
-					Invoice number
-				</label>
-				<input
-					className="form-control filters__input"
-					type="text"
-					placeholder="Enter value"
-				></input>
-				<label className="filters-text filters__content-label">
-					Time of arrival for unloading
-				</label>
-				<div className="filters__date">
-					<label className="filters-text filters__content-label--date">
-						from
-					</label>
-					<input
-						className="form-control filters__date-input--from"
-						type="date"
-					></input>
-					<label className="filters-text filters__content-label--date">
-						by
-					</label>
-					<input
-						className="form-control filters__date-input--by"
-						type="date"
-					></input>
-				</div>
-				<label className="filters-text filters__content-label">
-					Order type
-				</label>
-				<select className="form-select filters__select">
-					<option>RUED</option>
-					<option>RUEX</option>
-					<option>RUSG</option>
-				</select>
-			</div>
-		</div>
-	);
-};
+export default class Filters extends React.Component {
+	render() {
+		const {
+			filterArrivalFrom,
+			filterArrivalBy,
+			onFilterChange,
+		} = this.props;
 
-export default Filters;
+		return (
+			<div className="filters">
+				<div className="filters__heading">
+					<h2 className="filters-text filters__heading-label">
+						Filters
+					</h2>
+				</div>
+				<div className="filters__content">
+					<label className="filters-text filters__content-label">
+						ID
+					</label>
+					<input
+						className="form-control filters__input"
+						type="text"
+						placeholder="Enter value"
+					></input>
+					<label className="filters-text filters__content-label">
+						Invoice number
+					</label>
+					<input
+						className="form-control filters__input"
+						type="text"
+						placeholder="Enter value"
+					></input>
+					<label className="filters-text filters__content-label">
+						Time of arrival for unloading
+					</label>
+					<div className="filters__date">
+						<label className="filters-text filters__content-label--date">
+							from
+						</label>
+						<input
+							className="form-control filters__date-input--from"
+							type="date"
+							value={filterArrivalFrom}
+							onChange={(event) =>
+								onFilterChange(
+									event.target.value,
+									filterArrivalBy
+								)
+							}
+						></input>
+						<label className="filters-text filters__content-label--date">
+							by
+						</label>
+						<input
+							className="form-control filters__date-input--by"
+							type="date"
+							value={filterArrivalBy}
+							onChange={(event) =>
+								onFilterChange(
+									filterArrivalFrom,
+									event.target.value
+								)
+							}
+						></input>
+					</div>
+					<label className="filters-text filters__content-label">
+						Order type
+					</label>
+					<select className="form-select filters__select">
+						<option>RUED</option>
+						<option>RUEX</option>
+						<option>RUSG</option>
+					</select>
+				</div>
+			</div>
+		);
+	}
+}
